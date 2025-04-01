3a6e0430d823ccf1e9b221a4ff727582c6fb1e43
Alexandra Pana <pana.alexandra.06@gmail.com>
2025-04-01 20:52:41 +0300

    Added IMenu interface and AMenu abstract class

---
+++ src/interfaces/IMenu.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface IMenu {
+    void display();
+}
---
+++ src/models/menus/AMenu.java
@@ -0,0 +1,13 @@
+package models.menus;
+
+import interfaces.IMenu;
+
+public abstract class AMenu implements IMenu {
+    protected AMenu parentMenu;
+    public AMenu(AMenu parentMenu){this.parentMenu= parentMenu;}
+
+    @Override
+    public void display() {
+
+    }
+}
